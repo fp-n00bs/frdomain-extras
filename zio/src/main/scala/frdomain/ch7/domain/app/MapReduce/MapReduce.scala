e3f78fbda506b4dc21279816621eab198bde21e2
danp11 <Learngithub11>
2019-08-04 18:41:03 +0200

    x

--- zio/src/main/scala/frdomain/ch7/domain/app/MapReduce/MapReduce.scala
+++ zio/src/main/scala/frdomain/ch7/domain/app/MapReduce/MapReduce.scala
@@ -1,18 +1,13 @@
 package frdomain.ch7.domain.app.MapReduce
 
 import zio.{DefaultRuntime, Fiber, Queue, Ref, UIO, ZIO}
+import java.nio.file.{Files, Paths, Path}
+import scala.collection.JavaConverters._
+import zio.blocking._
 
-object MapReduce extends DefaultRuntime {
+object MapReduce extends App with DefaultRuntime {
 
-  def main(args: Array[String]): Unit = {
-    Step4.countAllWords("C:/Users/danp/Desktop/wordCount/", 5).sandbox.unit
-  }
-
-  object Step1 {
-
-    import java.nio.file.{Files, Paths, Path}
-    import scala.collection.JavaConverters._
-    import zio.blocking._
+  unsafeRun(countAllWords("C:/Users/Dan/Desktop/wordCount", 5))
 
     def listFiles(dir: String): ZIO[Blocking, Throwable, List[Path]] =
       effectBlocking {
@@ -27,24 +22,6 @@ object MapReduce extends DefaultRuntime {
         )
       }
 
-    def countAllWords(dir: String): ZIO[Blocking, Throwable, Long] =
-      for {
-        paths <- listFiles(dir)
-        counts <- ZIO.foreach(paths) { path =>
-          readContents(path).map { contents =>
-            contents.split(' ').size
-          }
-        }
-      } yield counts.sum
-
-
-  }
-
-  object Step2 {
-
-    import zio.blocking._
-    import Step1.{listFiles, readContents}
-
     def mapReduce[A, B](dir: String)
                        (map: String => A)
                        (z: B)(reduce: (B, A) => B): ZIO[Blocking, Throwable, B] =
@@ -56,36 +33,6 @@ object MapReduce extends DefaultRuntime {
           }
         }
       } yield mapped.foldLeft(z)(reduce)
-
-    def countAllWords(dir: String) = mapReduce(dir)(_.split(' ').size)(0L)(_ + _)
-  }
-
-  object Step3 {
-
-    import zio.blocking._
-    import Step1.{listFiles, readContents}
-
-    def mapReduce[A, B](dir: String, workers: Int)
-                       (map: String => A)
-                       (z: B)(reduce: (B, A) => B): ZIO[Blocking, Throwable, B] =
-      for {
-        paths <- listFiles(dir)
-        mapped <- ZIO.foreachParN(workers)(paths) { path =>
-          readContents(path).map { contents =>
-            map(contents)
-          }
-        }
-      } yield mapped.foldLeft(z)(reduce)
-
-    def countAllWords(dir: String, workers: Int) =
-      mapReduce(dir, workers)(_.split(' ').size)(0L)(_ + _)
-  }
-
-  object Step4 {
-
-    import java.nio.file.Path
-    import zio.blocking._
-    import Step1.{listFiles, readContents}
 
     def createMapWorker[A](inputQueue: Queue[Path], reduceQueue: Queue[A])(map: String => A) =
       (for {
@@ -139,6 +86,6 @@ object MapReduce extends DefaultRuntime {
           _.split(' ').size)(0L)(_ + _).fork
         _ <- printer(outputQueue)
       } yield ()
-  }
+
 
 }
